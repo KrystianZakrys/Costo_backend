e9915d66373fe27db25d1fbc4468a148c8788ff7
Krystian Zakrys <krystian.zakrys@altkomsoftware.pl>
2020-12-04 13:19:46 +0100

    Moved db context to another project.

---
+++ Costo.CommandQuery/CostoContext.cs
@@ -0,0 +1,29 @@
+﻿using Costo.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Costo.Infrastructure
+{
+    public class CostoContext :  DbContext
+    {
+        public DbSet<BankAccount> BankAccounts { get; set; }
+        public DbSet<SavingsGoal> SavingsGoals { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<TransactionCategory> TransactionCategories { get; set; }
+
+        public CostoContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        { 
+        }
+    }
+}
